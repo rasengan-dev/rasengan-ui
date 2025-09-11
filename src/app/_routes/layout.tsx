b044cf3e44e46bbaea46c5f07428c00a97956e4e
dilane3 <komboudilane125@gmail.com>
2025-09-11 22:23:28 +0100

    feat: adding analytics feature

--- src/app/_routes/layout.tsx
+++ src/app/_routes/layout.tsx
@@ -1,24 +1,21 @@
-import { Outlet, LayoutComponent, ScrollRestoration } from "rasengan";
+import { Outlet, LayoutComponent } from "rasengan";
 import { useTheme } from "@rasenganjs/theme";
 import { twMerge } from "tailwind-merge";
 import { Toaster } from "@/components/ui/sonner";
-import { useRef } from "react";
-// import { ScrollRestoration } from "@/components/common/molecules/scroll-restoration";
+import { Analytics } from "@vercel/analytics/react";
 
 const Layout: LayoutComponent = () => {
 	const { isDark } = useTheme();
 
-	const ref = useRef<HTMLDivElement>(null);
-
 	return (
 		<section
-			ref={ref}
 			className={twMerge(
 				"app-container w-screen min-h-screen overflow-x-hidden font-raleway",
 				isDark && "dark"
 			)}
 		>
-			{/* <ScrollRestoration alwaysToTop target={ref} /> */}
+			<Analytics framework='rasengan.js' />
+
 			<Outlet />
 			<Toaster />
 		</section>
