749bef55315c5c7b14e057c97b52df485c9f6bbf
dilane3 <komboudilane125@gmail.com>
2025-09-25 11:48:07 +0100

    feat: upgrading the preview component

--- src/app/_routes/preview/[category]/[group]/[type]/[component].page.tsx
+++ src/app/_routes/preview/[category]/[group]/[type]/[component].page.tsx
@@ -1,12 +1,16 @@
-import { SidebarBanner } from "@/components/common/layout/sidebar-banner";
-import ComponentItem from "@/components/features/component-item";
-import { PageComponent, useParams } from "rasengan";
+import { Link, PageComponent, useParams } from "rasengan";
 import { useComponentStore } from "@/store/components";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { ComponentCategoryLabel, ComponentType } from "@/data/components/type";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Laptop, Laptop2, Lock, Phone, Tablet } from "lucide-react";
+import { cn } from "@/lib/utils";
+import ThemeButton from "@/components/common/atoms/theme-button";
 
 const Page: PageComponent = () => {
 	const { components } = useComponentStore();
+
+	const [isMobile, setIsMobile] = useState(false);
 
 	const {
 		category,
@@ -51,8 +55,75 @@ const Page: PageComponent = () => {
 	}
 
 	return (
-		<section className='relative h-screen w-screen overflow-hidden'>
-			<iframe src={`${component.link}`} className='w-full h-full'></iframe>
+		<section className='bg-background relative h-screen w-screen overflow-hidden p-4'>
+			<div className='flex h-[3rem] items-center justify-between'>
+				<div>
+					<div className='text-foreground flex gap-1'>
+						<span>{category}</span>
+						{"/"}
+						<span>{groupName}</span>
+						{"/"}
+						<span>{typeName}</span>
+						{"/"}
+						<span>{componentName}</span>
+					</div>
+					<span className='text-muted-foreground text-sm'>
+						{component.name}
+					</span>
+				</div>
+
+				<div>
+					<div className='flex items-center gap-2'>
+						<Button
+							size='icon'
+							variant='outline'
+							onClick={() => setIsMobile(false)}
+							className={cn(
+								"text-foreground",
+								!isMobile &&
+									"bg-primary text-primary-foreground dark:bg-primary dark:text-primary-foreground hover:bg-primary/80 hover:text-primary-foreground dark:hover:bg-primary/80 dark:hover:text-primary-foreground"
+							)}
+						>
+							<Laptop2 />
+						</Button>
+						<Button
+							size='icon'
+							variant='outline'
+							onClick={() => {
+								setIsMobile(true);
+							}}
+							className={cn(
+								"text-foreground",
+								isMobile &&
+									"bg-primary text-primary-foreground dark:bg-primary dark:text-primary-foreground hover:bg-primary/80 hover:text-primary-foreground dark:hover:bg-primary/80 dark:hover:text-primary-foreground"
+							)}
+						>
+							<Tablet />
+						</Button>
+					</div>
+				</div>
+
+				<div className='flex items-center gap-2'>
+					<Link to='/pricing' className='w-full sm:w-auto'>
+						<Button
+							className='w-full sm:w-auto'
+							// variant='outline'
+						>
+							<Lock />
+							<span>Get Code</span>
+						</Button>
+					</Link>
+					<ThemeButton />
+				</div>
+			</div>
+			<div className='w-full h-[calc(100vh-6rem)] overflow-hiddenf'>
+				<iframe
+					src={`${component.link}?view=preview`}
+					className={`${
+						isMobile ? "w-[375px] h-[667px] scale-95" : "w-full h-full"
+					} border border-border rounded-lg mt-4 mx-auto transition-all duration-300`}
+				></iframe>
+			</div>
 		</section>
 	);
 };
